7717a796e30390b75c53ed221f8b0935160c1fc3
Marta Sousa <marta.sousa@gmail.com>
2019-11-14 21:52:50 -0200

    criando class User em models para cadastro de novos usuários no banco de dados

---
+++ models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "Conexao.php";
+
+class User extends Conexao {
+
+  // Função para cadastrar novo usuário
+  public function addUser($firstName,$lastName,$username,$encPass){
+
+    $db = parent::criarConexao();
+    $query = $db->prepare("INSERT INTO users(firstName,lastName,username,encPass) VALUES (:firstName,:lastName,:username,:encPass)");
+    return $query->execute([
+      "firstName" => $firstName,
+      "lastName" => $lastName,
+      "username" => $username,
+      "encPass" => $encPass]);
+  }
+
+}
